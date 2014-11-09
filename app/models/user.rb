7132dbec73cecfe068f7470481821cf95726228e
Max Sveshnikov <msveshnikov@thumbtack.net>
2014-11-09 23:49:11 +0600

    passwords secure

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,5 +7,6 @@ class User < ActiveRecord::Base
   validates :email, presence: true, format: {with: VALID_EMAIL_REGEX},
             uniqueness: true
 
+  validates :password, length: {minimum: 6}
   has_secure_password
 end
