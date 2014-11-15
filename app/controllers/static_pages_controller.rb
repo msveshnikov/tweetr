0326dc838c74e87da058fce37cc247d11afc6c51
Max Sveshnikov <msveshnikov@thumbtack.net>
2014-11-15 23:31:51 +0600

    last touches

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -3,6 +3,8 @@ class StaticPagesController < ApplicationController
     if signed_in?
       @micropost = current_user.microposts.build
       @feed_items = current_user.feed.paginate(page: params[:page], per_page: 5)
+    else
+      @feed_items = Micropost.paginate(page: params[:page], per_page: 10)
     end
   end
 
